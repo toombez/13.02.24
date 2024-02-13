5d01e5e24760bc10fc25a6e6fd89bdafd365b9fd
Timur <toombez.timurtokaev@gmail.com>
2024-02-13 11:22:19 +0300

    Render Posts from backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,24 @@
+import { useState, useEffect } from "react"
+import Post from "./Post"
+import { ofetch } from 'ofetch'
+
 function App() {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        ofetch('http://127.0.0.1:5000/api/post/')
+            .then((r) => {
+                console.log(r);
+
+                setPosts(() => r?.objects || [])
+            })
+    }, [])
+
     return (
         <div>
+            {posts.map((post) => <Post post={post} />)}
         </div>
     )
 }
+
+export default App
