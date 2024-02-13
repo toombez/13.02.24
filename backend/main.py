57d194d520efdf304af3b664b58bb34942eb3dc5
Timur <toombez.timurtokaev@gmail.com>
2024-02-13 11:02:05 +0300

    Add Post model

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,14 @@
 from flask import Flask
+from flask_peewee.db import SqliteDatabase
+import peewee
+
+db = SqliteDatabase('app.db')
+
+class Post(db.Model):
+    id = peewee.PrimaryKeyField()
+    title = peewee.CharField()
+    content = peewee.CharField()
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '123'
 if (__name__ == '__main__'):
