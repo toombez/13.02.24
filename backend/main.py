a643e6395d9c02082d04d480c0e833e51817b32e
Timur <toombez.timurtokaev@gmail.com>
2024-02-13 11:02:58 +0300

    Add Admin

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_peewee.db import SqliteDatabase
 from flask_peewee.rest import RestAPI
+from flask_admin import Admin
+from flask_admin.contrib.peewee import ModelView
 import peewee
 
 db = SqliteDatabase('app.db')
@@ -10,13 +12,20 @@ class Post(db.Model):
     title = peewee.CharField()
     content = peewee.CharField()
 
+class PostAdminView(ModelView):
+    pass
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '123'
+
 if (__name__ == '__main__'):
     try:
         Post.create_table()
     except:
         pass
+
+    admin = Admin(app)
+    admin.add_view(PostAdminView(Post))
 
     api = RestAPI(app)
     api.register(Post)
